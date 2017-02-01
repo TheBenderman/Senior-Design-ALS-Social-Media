a6fbc4f008464faffa173a9f8b2b2f1fd426653c
Kevin Garrone <kevin7571@yahoo.com>
2017-02-01 16:50:51 -0500

    Created Custom Unity Editor Function to create new scenes and/or create common objects.

---
+++ connectome-unity/Assets/Editor/Connectome/ConnectomeEditorTools.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+/// <summary>
+/// Defines cusom editor functionality exclusive to Connectome
+/// </summary>
+public class ConnectomeEditorTools : EditorWindow {
+    /// <summary>
+    /// Create a new Unity Scene and insert all common prefabs.
+    /// </summary>
+    [MenuItem("Connectome/Create Connectome Scene")]
+    static void CreateConnectomeScene()
+    {
+        UnityEditor.SceneManagement.EditorSceneManager.NewScene(UnityEditor.SceneManagement.NewSceneSetup.DefaultGameObjects);
+        GenerateCommonObjects();
+    }
+
+    /// <summary>
+    /// Add the common prefabs to the current scene.
+    /// </summary>
+    [MenuItem("Connectome/Generate Common Objects")]
+    static void GenerateCommonObjects()
+    {
+        if (!GameObject.Find("ConnectomeScene"))
+        {
+            GameObject connectomeScene = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefab/ConnectomeScene.prefab"));
+            connectomeScene.name = "ConnectomeScene";
+        }
+    }
+}
