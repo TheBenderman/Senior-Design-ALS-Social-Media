cf2aeb84607ec2dcfb7ae8267117219c5d8cd027
kennanmeyer <meyer.kennan@gmail.com>
2017-02-02 15:19:50 -0500

    Initial Keyboard Implementation

---
+++ connectome-unity/Assets/Common Script/KeyboardManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardManager : MonoBehaviour {
+    GameObject Keyboard;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setKeyboard (string keyboardtype)
+    {
+        Keyboard = Instantiate(Resources.Load(keyboardtype),GameObject.Find("Canvas").transform) as GameObject;
+        GameObject.FindGameObjectWithTag("Exit").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            removeKeyboard();
+        });
+    }
+
+    public void removeKeyboard()
+    {
+        Destroy(Keyboard.gameObject);
+    }
+}
