e4677970886c15425ef6053dfaedd3f188d9e22c
KLD0093 <KLD0093@hotmail.com>
2017-02-07 18:22:55 -0500

    Added a popup test scene that demonstrate getting string from a popup.
    Location: Assets\Scene\Test\KLDTest\PromptTest

---
+++ connectome-unity/Assets/Scene/Test/KLDTest/PromptTest/PromptPopup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Connectome.KLD.PromptTest
+{
+    public class PromptPopup : MonoBehaviour
+    {
+        /// <summary>
+        /// Contains inputted text 
+        /// </summary>
+        public InputField InputField;
+
+        /// <summary>
+        /// Invoked when submitting popup 
+        /// </summary>
+        Action<string> OnSubmit;
+
+        /// <summary>
+        /// Called from outside of class 
+        /// </summary>
+        /// <param name="onSubmit"></param>
+        public void Popup(Action<string> onSubmit)
+        {
+            OnSubmit = onSubmit;
+            Display();
+        }
+
+        /// <summary>
+        /// Called by SubmitButton within Popup
+        /// </summary>
+        public void Submit()
+        {
+            Hide();
+            OnSubmit(InputField.text);
+        }
+
+        /// <summary>
+        /// Display popup 
+        /// </summary>
+        private void Display()
+        {
+            gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// Hide popup 
+        /// </summary>
+        private void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
