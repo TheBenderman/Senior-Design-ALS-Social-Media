511113d565a3df104295d369b1bc696e83d98692
Jon Schuck <isceith@gmail.com>
2017-03-05 22:50:20 -0500

    Added Logger Interface and CSV Logger

---
+++ connectome-csharp/CalibrationAPI/Loggers/CsvLogger.cs
@@ -0,0 +1,32 @@
+﻿using Connectome.Calibration.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Connectome.Calibration.API.Loggers
+{
+    public class CsvLogger : LoggerInterface
+    {
+        private String fileName;
+        private String csvLine;
+
+        public CsvLogger(String filename)
+        {
+            fileName = filename;
+            csvLine = DateTime.Now + ",";
+        }
+
+        public void add(String unit)
+        {
+            csvLine += unit +",";
+        }
+
+        public void write()
+        {
+            File.AppendAllText(fileName, csvLine.Substring(0,csvLine.Length - 1) +"\n");
+            csvLine = "";
+        }
+    }
+}
