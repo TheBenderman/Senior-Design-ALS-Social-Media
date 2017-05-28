57bdf8d233bfc530b4c95686c4d9af16573cf6df
Nazmul Hassan <solergiga@yahoo.com>
2017-05-28 18:09:37 -0400

    Unity UI test preperations(?)
    Signed-off-by: Nazmul Hassan <solergiga@yahoo.com>

---
+++ connectome-unity/Assets/UITest/TwitterUI/HomeObjects.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HomeObject : MonoBehaviour 
+{
+    [SerializeField] GameObject KeyboardPrefab;
+	[SerializeField] GameObject secondScreenPrefab;
+
+
+    void Start()
+    {
+        this.Inject();
+    }
+
+    public void OpenSecondScreen()
+    {
+        var s = Object.Instantiate(secondScreenPrefab);
+        s.name = secondScreenPrefab.name;
+        s.transform.SetParent(transform.parent, false);
+    }
+}
