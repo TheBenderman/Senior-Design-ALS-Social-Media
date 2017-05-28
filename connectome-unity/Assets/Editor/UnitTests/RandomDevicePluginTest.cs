92b1f8484d84de17f784164adaffa78929c9a466
Jon Schuck <isceith@gmail.com>
2017-05-28 16:47:28 -0400

    Random Device Plugin Unit Test

---
+++ connectome-unity/Assets/Editor/UnitTests/RandomDevicePluginTest.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using Connectome.Unity.Plugin;
+using Connectome.Emotiv.Implementation;
+
+[TestFixture]
+public class RandomDevicePluginTest {
+
+    RandomDevicePlugin rdp;
+
+    [SetUp]
+    public void init()
+    {
+        rdp = new RandomDevicePlugin();
+        rdp.Setup();
+    }
+
+	[Test]
+    [Category("Setup Tests")]
+    public void SetupTest() {
+        Assert.IsInstanceOf<RandomEmotivDevice>(rdp.Content);
+	}
+
+    [Test]
+    [Category("Is Connected Tests")]
+    public void IsConnectedTest()
+    {
+        Assert.IsFalse(rdp.IsConnected);
+    }
+
+    [Test]
+    [Category("Battery Level Tests")]
+    public void BatteryLevelTest()
+    {
+        Assert.AreEqual(0, rdp.BatteryLevel);
+    }
+
+    [Test]
+    [Category("Wireless Signal Strength Tests")]
+    public void WirelessSignalStrengthTest()
+    {
+        Assert.AreEqual(0, rdp.WirelessSignalStrength);
+    }
+
+    [Test]
+    [Category("Connect Disconnect Tests")]
+    public void ConnectDisconnectTest()
+    {
+        rdp.Connect();
+        Assert.IsTrue(rdp.IsConnected);
+        rdp.Disconnect();
+        Assert.IsFalse(rdp.IsConnected);
+    }
+
+}
