eeb180f1d0e12353ff14b94a031b3da15e94b7a9
Jon Schuck <isceith@gmail.com>
2017-05-28 21:01:11 -0400

    Flashing Controller Unit Test

---
+++ connectome-unity/Assets/Editor/UnitTests/FlashingControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using Assets.UITestComponents;
+
+
+[TestFixture]
+public class FlashingControllerTest {
+
+    FlashingController fc;
+
+    [SetUp]
+    public void init()
+    {
+        fc = new FlashingController();
+        fc.high = new Connectome.Unity.UI.FlashingHighlighter();
+        fc.text = new DummyText();
+    }
+
+	[Test]
+    [Category("Update Value Tests")]
+    public void UpdateValueTest() {
+        fc.UpdateValue(new DummySlider());
+        Assert.AreEqual(1, fc.high.Frequency);
+        Assert.AreEqual("1", fc.text.text);
+	}
+}
